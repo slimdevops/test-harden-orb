32ff739b5fa675ad9e6c444d38aada8027315d9f
Sreejith <sreejiths@focaloid.com>
2023-04-13 14:30:45 +0530

    run update

--- src/scripts/run.sh
+++ src/scripts/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 IMAGE="${SOURCEIMAGE}.instrumented"
-docker pull $IMAGE
+docker pull "$IMAGE"
 
 eval "docker run -d --cap-add ALL --user root --name my-orb-project -p 9001:9001 $IMAGE"

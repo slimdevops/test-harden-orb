41aa7e353d4d3aac78517ddc7f37f35f2abdfcd8
Sreejith <sreejiths@focaloid.com>
2023-04-13 14:53:32 +0530

    run update

--- src/scripts/run.sh
+++ src/scripts/run.sh
@@ -1,5 +1,40 @@
 #!/bin/bash
-IMAGE="${SOURCEIMAGE}.instrumented"
+
+
+BASEIMAGE="${SOURCEIMAGE}"
+
+string="${SOURCECONNECTOR}/${SOURCEIMAGE}"
+
+match=$(echo "${string}" | grep -oP '^(?:([^/]+)/)?(?:([^/]+)/)?([^@:/]+)(?:[@:](.+))?$')
+
+IFS='/' 
+read -r -a parts <<< "$match"
+
+
+namespace=${parts[1]}
+repository=${parts[2]}
+
+if [ -z "$repository" ]; then
+  repository="${namespace}"
+  namespace="library"
+fi
+
+if echo "$repository" | grep -q ":"; then
+  IFS=':' read -ra arr <<< "$repository"
+  tag=${arr[1]}
+  repository=${arr[0]}
+else
+  tag="latest"
+fi
+
+
+
+
+if [ -z "$namespace" ]; then
+  namespace="library"
+fi
+
+IMAGE="${namespace}/${repository}:${tag}.instrumented"
 docker pull "$IMAGE"
 
 eval "docker run -d --cap-add ALL --user root --name my-orb-project -p 9001:9001 $IMAGE"

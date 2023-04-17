4b0e750f27e0e7aac6610ce5341681092fd3b3e7
Sreejith <sreejiths@focaloid.com>
2023-04-17 09:00:47 +0530

    Env test

--- src/scripts/cli-harden.sh
+++ src/scripts/cli-harden.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Hardening workflow: $WORKFLOW_ID"
 echo "$BASH_ENV"
+echo "${API_TOKEN}"
 log_output=$(slim harden --id "$WORKFLOW_ID" 2>&1 | tee /dev/stderr)
 
 if [[ $log_output =~ \[instrument\]\ completed ]]; then
